57611e02d635c96baac186eb77a1541c1c3eb6bb
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-30 02:34:40 +0530

    Create MonitorBatteryCharging_prv.h

---
+++ MonitorBatteryCharging_prv.h
@@ -0,0 +1,21 @@
+#ifndef  MONITORBATTERYCHARGING_PRV_
+#define  MONITORBATTERYCHARGING_PRV_
+
+/* Enum declarations */
+typedef enum
+{
+  OK_VALID;
+  ERROR_INVALID;
+}retChargeReadingStatus_en;
+/* Structure declarations */
+typedef struct
+{
+  int continuousReadingCnt;
+  int totalReadingsCnt;
+  retChargeReadingStatus_en paramStatus;
+}batteryChargeReading_st;
+
+
+
+/* function declarations */
+batteryChargeReading_st checkBatteryChargeReading(int *chargeReading, int numOfReadings);
