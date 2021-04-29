1fc1843c825fc66e8ec313087df1e1dd3a9993c9
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-30 03:32:09 +0530

    Update MonitorBatteryCharging.c

--- MonitorBatteryCharging.c
+++ MonitorBatteryCharging.c
@@ -6,7 +6,7 @@ batteryChargeReading_st checkBatteryChargeReading(int *chargeReading, int numOfR
   int loopCntr = 0;
   int continuousReading =1, tempVar;
   batteryChargeReading_st batteryChargeDetails;
-  batteryChargeDetails.paramStatus = OK_VALID;
+  batteryChargeDetails.paramStatus = ERROR_INVALID;
   tempVar = chargeReading[loopCntr];
   for(;loopCntr<numOfReadings;loopCntr++)
   {
@@ -15,11 +15,12 @@ batteryChargeReading_st checkBatteryChargeReading(int *chargeReading, int numOfR
       continuousReading++; /* denotes continuous reading are present */
     }
     tempVar = chargeReading[loopCntr+1];
+    batteryChargeDetails.paramStatus = OK_VALID;
   }
-  if((loopCntr == 0) || (chargeReading == NULL))
-  {
-    batteryChargeDetails.paramStatus = ERROR_INVALID;
-  }
+//  if((loopCntr == 0) || (chargeReading == NULL))
+//  {
+//   batteryChargeDetails.paramStatus = ERROR_INVALID;
+//  }
   batteryChargeDetails.continuousReadingCnt = continuousReading;
   batteryChargeDetails.totalReadingsCnt = loopCntr;
   return batteryChargeDetails;
