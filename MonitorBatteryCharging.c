b5e51b29195cd9e40d418d9ef97934433c02ef51
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-30 04:26:58 +0530

    Update MonitorBatteryCharging.c

--- MonitorBatteryCharging.c
+++ MonitorBatteryCharging.c
@@ -5,18 +5,24 @@ batteryChargeReading_st checkBatteryChargeReading(int *chargeReading, int numOfR
 {
   int loopCntr = 0;
   int continuousReading =1, tempVar;
+  int chargeCntr = 1;
   batteryChargeReading_st batteryChargeDetails;
   batteryChargeDetails.paramStatus = ERROR_INVALID;
   tempVar = chargeReading[loopCntr];
-  for(;loopCntr<numOfReadings;loopCntr++)
+  for(;loopCntr<(numOfReadings-1);loopCntr++)
   {
     if((tempVar+1) == chargeReading[loopCntr+1])
     {
       continuousReading++; /* denotes continuous reading are present */
+      /* these below mwntioned array stores the set of continuous values recorded */
+      batteryChargeDetails.continuousChargReadValue[chargeCntr-1] =  tempVar;
+      batteryChargeDetails.continuousChargReadValue[chargeCntr] =  tempVar+1;
+      chargeCntr+=2;
     }
     tempVar = chargeReading[loopCntr+1];
     batteryChargeDetails.paramStatus = OK_VALID;
   }
+  /* this below mwntioned code can be tested/ checked directly checked based on loopCntr value, hence not checked */
 //  if((loopCntr == 0) || (chargeReading == NULL))
 //  {
 //   batteryChargeDetails.paramStatus = ERROR_INVALID;
