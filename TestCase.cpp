9d33028c174a64780b79304ddff07cf159872da2
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-30 03:16:10 +0530

    Create TestCase.cpp

---
+++ TestCase.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "MonitorBatteryCharging_prv.h"
+
+TEST_CASE("To check Continous readings in given range") {
+    int CurrentChargeRange[] = {1,2,5,4,6,8,7,9};
+    batteryChargeReading_st batteryChargeDetails;
+    int sizeOfArrayIndex = sizeof(CurrentChargeRange) / sizeof(CurrentChargeRange[0]);
+  
+  batteryChargeDetails = checkBatteryChargeReading(&CurrentChargeRange[0],sizeOfArrayIndex);
+  REQUIRE(batteryChargeDetails.continuousReadingCnt == 2);
+}
+
+TEST_CASE("To check if Current reading range is empty") 
+{
+    int CurrentChargeRange[] = {};
+    batteryChargeReading_st batteryChargeDetails;
+    int sizeOfArrayIndex = sizeof(CurrentChargeRange) / sizeof(CurrentChargeRange[0]);
+  
+  batteryChargeDetails = checkBatteryChargeReading(&CurrentChargeRange[0],sizeOfArrayIndex);
+  REQUIRE(batteryChargeDetails.paramStatus == ERROR_INVALID);
+}
